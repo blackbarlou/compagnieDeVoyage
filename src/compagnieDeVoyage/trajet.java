14845680cc0b9c8104ea7b86f8d5923a36d103f2
fashola <nurudeen.fashola@uqtr.ca>
2024-05-23 21:26:24 -0400

    ajout de la classe trajet

---
+++ src/compagnieDeVoyage/trajet.java
@@ -0,0 +1,89 @@
+package compagnieDeVoyage;
+
+
+/*
+Cette Classe represente un trajet
+ */
+public class trajet {
+        private String villeDepart;
+        private String villeArrive;
+        private int kilometrageDepart;
+        private int kilometrageArrive;
+        private Bus bus;
+
+        /**
+         * constructeur de la classe trajet
+         * @param villeDepart : represente la ville de depart
+         * @param villeArrive : represnete la ville d'arrivee
+         * @param kilometrageDepart : represente le kilometrage du bus au depart du trajet
+         * @param kilometrageArrive : represente le kilometrage du bus a l'arrive du trajet
+         * @param bus : represente le bus utiliser pour effectuer le trajet
+         */
+        public trajet(String villeDepart, String villeArrive, int kilometrageDepart, int kilometrageArrive, Bus bus ){
+            this.villeArrive = villeArrive;
+            this.villeDepart = villeDepart;
+            this.kilometrageArrive = kilometrageArrive;
+            this.kilometrageDepart = kilometrageDepart;
+            this.bus = bus;
+        }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public void setBus(Bus bus) {
+        this.bus = bus;
+    }
+
+    public int getKilometrageArrive() {
+        return kilometrageArrive;
+    }
+
+    public void setKilometrageArrive(int kilometrageArrive) {
+        this.kilometrageArrive = kilometrageArrive;
+    }
+
+    public int getKilometrageDepart() {
+        return kilometrageDepart;
+    }
+
+    public void setKilometrageDepart(int kilometrageDepart) {
+        this.kilometrageDepart = kilometrageDepart;
+    }
+
+    public String getVilleArrive() {
+        return villeArrive;
+    }
+
+    public void setVilleArrive(String villeArrive) {
+        this.villeArrive = villeArrive;
+    }
+
+    public String getVilleDepart() {
+        return villeDepart;
+    }
+
+    public void setVilleDepart(String villeDepart) {
+        this.villeDepart = villeDepart;
+    }
+
+    @Override
+    public String toString() {
+        return "trajet [" +
+                "ville de Depart : " + villeDepart + '\'' +
+                ", ville d'Arrive :" + villeArrive + '\'' +
+                ", kilometrage au Depart :" + kilometrageDepart +
+                ", kilometrage l'Arrive :" + kilometrageArrive +
+                ", bus du trajet: " + bus +
+                ']';
+    }
+
+    public void afficherLesCaracteristiques ( ) {
+        System.out.println("Les caracteristiques du trajet :");
+        System.out.println("La ville de Depart : " + villeDepart);
+        System.out.println("La ville de D'arrivee : " + villeArrive);
+        System.out.println("kilometrage au Depart :" + kilometrageDepart);
+        System.out.println("kilometrage a l'Arrive :" + kilometrageArrive);
+        System.out.println("Le bus du trajet : " + bus);
+    }
+}
