b839ae55b68d887b59e9f896a72708efa7f19001
moucheuW <handy.farex.moucheu.wafo@uqtr.ca>
2024-05-27 16:06:22 -0400

    Récupération de la Classe principal Main

--- module1/src/compagnieDeVoyage/Main.java
+++ module1/src/compagnieDeVoyage/Main.java
@@ -4,28 +4,7 @@ package compagnieDeVoyage;
 public class Main {
 
     public static void main(String[] args) throws MauvaisFormatException, ValeurNulleException {
-       /*Compagnie compagnie = new Compagnie("STTR");
-        *//*try {
-            //compagnie.creerBus();
-            compagnie.creerTrajet();
-            } catch ( ValeurNulleException exc ){
-            System.out.println("erreur rencontree " + exc.getMessage());
-        } catch ( MauvaisFormatException m ){
-            System.out.println(" Veillez reprendre " + m.getMessage());
-            compagnie.creerTrajet();
-        }*//*
-        //Chauffeur chauffeur = new Chauffeur("ben","Nour","hghdyu",24,2013);
-        //System.out.println(chauffeur);
-        //compagnie.reservation();
-        Chauffeur chauffeur = new Chauffeur("Ben", "Franck", "670 rue des mangues", 20, 2034);
-        //Chauffeur[] listec = new Chauffeur[1];
-        Bus bus = new Bus("ijjd","noire",120,2000);
-        compagnie.setListeDeBus(bus);
-        Trajet trajet = new Trajet("Douala", "Yaounde",123,234, bus);
-        chauffeur.setTrajetChauffeur(trajet);
-        compagnie.setListeDeChauffeur(chauffeur);
-        compagnie.listeDeBusConduitParChauffeur();
-*/
+
         Menu menu = new Menu();
         menu.menuPrincipal();
 
