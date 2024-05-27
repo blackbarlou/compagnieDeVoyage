94ba0c8839d9ffb1d842ee97646ec73ddf65e33c
Michael FOMKOUNG <michael.cedric.fomkoung.tagne@uqtr.com>
2024-05-27 23:09:56 +0100

    correction du bug lors de la recuperation de la valeur capaciteReservoir dans la methode creerBus()

--- module1/src/compagnieDeVoyage/Compagnie.java
+++ module1/src/compagnieDeVoyage/Compagnie.java
@@ -78,7 +78,7 @@ public class Compagnie {
             nombreDePlace = scan.nextInt();
             scan.nextLine();
             System.out.println("Donnez la capacite du reservoir");
-            capaciteDuReservoir = scan.nextDouble();
+            capaciteDuReservoir = Float.parseFloat(scan.nextLine());
         } catch ( InputMismatchException exc ){
             System.out.println("le Nombre de place doit etre un entier et la capacite un reel veillez recommencer SVP");
             creerBus();
